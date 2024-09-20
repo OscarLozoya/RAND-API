b92a7f83964b1ae0a79de48f868a5f897b2c0799
Ivan Lozoya <lozoya.oscar.ivan@gmail.com>
2024-09-19 20:50:06 -0600

    inital commit with spring initializr

---
+++ src/main/java/com/lori/Rand_API/RandApiApplication.java
@@ -0,0 +1,13 @@
+package com.lori.Rand_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandApiApplication.class, args);
+	}
+
+}
