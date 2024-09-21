222bef58c511b2d1d798efc57d6d608c2505f984
Ivan Lozoya <lozoya.oscar.ivan@gmail.com>
2024-09-20 22:44:33 -0600

    add method for consult Fibonacci with and without param, two method for calculated

--- src/main/java/com/lori/Rand_API/domain/service/MathCalculusService.java
+++ src/main/java/com/lori/Rand_API/domain/service/MathCalculusService.java
@@ -2,12 +2,15 @@ package com.lori.Rand_API.domain.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MathCalculusService {
 
     public String staticFibonacci(){
-        int fIterations = 50;
-        String result = "0,";
+        int fIterations = 15;
+        String result = "0, 1,";
         int current = 0;
         int next = 1;
         int operation;
@@ -20,5 +23,21 @@ public class MathCalculusService {
         }
         System.out.println(result);
         return result;
+    }
+
+    public String fibonacci(int nums){
+        List<Long> result = new ArrayList<>();
+        long current = 0;
+        long next = 1;
+        long operation;
+        result.add(current);
+        result.add(next);
+        for(int i = 0; i < nums; i++){
+            operation = current + next;
+            result.add(operation);
+            current =  next;
+            next = operation;
+        }
+        return result.toString();
     }
 }
--- src/main/java/com/lori/Rand_API/web/controller/MathController.java
+++ src/main/java/com/lori/Rand_API/web/controller/MathController.java
@@ -1,14 +1,20 @@
 package com.lori.Rand_API.web.controller;
 
+import com.lori.Rand_API.domain.service.MathCalculusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/math-calculus")
 public class MathController {
+    @Autowired
+    private MathCalculusService calculusService;
+
     @GetMapping("/")
     public ResponseEntity<String> sayHellobro(){
         return new ResponseEntity<String>("Hello controller context", HttpStatus.OK);
@@ -16,6 +22,11 @@ public class MathController {
 
     @GetMapping("/fibonacciOf5")
     public ResponseEntity<String> getFibonnaci(){
-        return new ResponseEntity<>(String.valueOf(5), HttpStatus.OK);
+        return new ResponseEntity<>(calculusService.staticFibonacci(), HttpStatus.OK);
+    }
+
+    @GetMapping("/fibonacci/{iterations}")
+    public ResponseEntity<String> getFibonnaci(@PathVariable("iterations") int interation){
+        return new ResponseEntity<>(calculusService.fibonacci(interation), HttpStatus.OK);
     }
 }
